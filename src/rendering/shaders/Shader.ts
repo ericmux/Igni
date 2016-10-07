36afdbe449a0a3ed116e3bb0932892588f597017
Eric Muxagata <ericgmconrado@gmail.com>
2016-10-07 04:27:46 -0300

    abstract Shader class capable of loading any pair of shaders.

--- src/rendering/shaders/Shader.ts
+++ src/rendering/shaders/Shader.ts
@@ -4,41 +4,38 @@ abstract class Shader {
     protected program: WebGLProgram;
     protected gl_context: WebGLRenderingContext;
 
-    protected compileProgram(vertex_shader_source :string, fragment_shader_source :string) :void {
-
+    constructor(vertex_shader_source: string, fragment_shader_source: string) {
         // Load and compile vertex shader.
-        this.vertex_shader = this.gl_context.createShader( this.gl_context.VERTEX_SHADER );
-        this.gl_context.shaderSource( this.vertex_shader, vertex_shader_source );
-        this.gl_context.compileShader( this.vertex_shader );
-        if ( !this.gl_context.getShaderParameter(this.vertex_shader, this.gl_context.COMPILE_STATUS) ) {
+        this.vertex_shader = this.gl_context.createShader(this.gl_context.VERTEX_SHADER);
+        this.gl_context.shaderSource(this.vertex_shader, vertex_shader_source);
+        this.gl_context.compileShader(this.vertex_shader);
+        if (!this.gl_context.getShaderParameter(this.vertex_shader, this.gl_context.COMPILE_STATUS)) {
             let msg = "Vertex shader failed to compile.  The error log is:"
-            + "<pre>" + this.gl_context.getShaderInfoLog( this.vertex_shader ) + "</pre>";
+                + "<pre>" + this.gl_context.getShaderInfoLog(this.vertex_shader) + "</pre>";
             throw new Error(msg);
         }
 
-        this.fragment_shader = this.gl_context.createShader( this.gl_context.FRAGMENT_SHADER );
-        this.gl_context.shaderSource( this.fragment_shader, fragment_shader_source );
-        this.gl_context.compileShader( this.fragment_shader );
-        if ( !this.gl_context.getShaderParameter(this.fragment_shader, this.gl_context.COMPILE_STATUS) ) {
+        // Load and compile fragment shader.
+        this.fragment_shader = this.gl_context.createShader(this.gl_context.FRAGMENT_SHADER);
+        this.gl_context.shaderSource(this.fragment_shader, fragment_shader_source);
+        this.gl_context.compileShader(this.fragment_shader);
+        if (!this.gl_context.getShaderParameter(this.fragment_shader, this.gl_context.COMPILE_STATUS)) {
             let msg = "Fragment shader failed to compile.  The error log is:"
-            + "<pre>" + this.gl_context.getShaderInfoLog( this.fragment_shader ) + "</pre>";
-            alert( msg );
-            return null;
+                + "<pre>" + this.gl_context.getShaderInfoLog(this.fragment_shader) + "</pre>";
+            throw new Error(msg);
         }
 
-        let program = this.gl_context.createProgram();
-        this.gl_context.attachShader( program, this.vertex_shader );
-        this.gl_context.attachShader( program, this.fragment_shader );
-        this.gl_context.linkProgram( program );
-        
-        if ( !this.gl_context.getProgramParameter(program, this.gl_context.LINK_STATUS) ) {
+        // Link program
+        this.program = this.gl_context.createProgram();
+        this.gl_context.attachShader(this.program, this.vertex_shader);
+        this.gl_context.attachShader(this.program, this.fragment_shader);
+        this.gl_context.linkProgram(this.program);
+
+        if (!this.gl_context.getProgramParameter(this.program, this.gl_context.LINK_STATUS)) {
             var msg = "Shader program failed to link.  The error log is:"
-                + "<pre>" + this.gl_context.getProgramInfoLog( program ) + "</pre>";
-            alert( msg );
-            return null;
+                + "<pre>" + this.gl_context.getProgramInfoLog(this.program) + "</pre>";
+            throw new Error(msg);
         }
-
-        return program;
     }
 }
 export default Shader;
