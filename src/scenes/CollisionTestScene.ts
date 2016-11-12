f68152a467b35ab20c88317464bbb9273354e1ee
Eric Muxagata <ericgmconrado@gmail.com>
2016-11-12 18:53:06 -0200

    Added test scene with gravity and lots of bodies.

--- src/scenes/CollisionTestScene.ts
+++ src/scenes/CollisionTestScene.ts
@@ -30,25 +30,25 @@ export default class CollisionTestScene extends TestScene {
             (<RectangleShape>collisionManifold.bodyB.shape).color = color;
         };
 
-        // // PAIR 1
-        // // Add a circular body.
-        // let body1 : Body = new CircularBody(<CircularBodyDefinition>{
-        //     radius: 30,
-        //     position: vec2.fromValues(0,60),
-        //     mass: 1.0,
-        //     velocity: vec2.fromValues(-30.0, 0),
-        //     torque: 5.0
-        // });
-        // game.addBody(body1);
+        // PAIR 1
+        // Add a circular body.
+        let body1 : Body = new CircularBody(<CircularBodyDefinition>{
+            radius: 30,
+            position: vec2.fromValues(0,60),
+            mass: 1.0,
+            velocity: vec2.fromValues(-30.0, 0),
+            torque: 5.0
+        });
+        game.addBody(body1);
 
-        // // Add a circular body.
-        // let body2 : Body = new CircularBody(<CircularBodyDefinition>{
-        //     position: vec2.fromValues(-50,60),
-        //     radius: 10,
-        //     mass: 1.0,
-        //     velocity: vec2.fromValues(30.0, 0.0)
-        // });
-        // game.addBody(body2);  
+        // Add a circular body.
+        let body2 : Body = new CircularBody(<CircularBodyDefinition>{
+            position: vec2.fromValues(-50,60),
+            radius: 10,
+            mass: 1.0,
+            velocity: vec2.fromValues(30.0, 0.0)
+        });
+        game.addBody(body2);  
 
         // PAIR 2
         // Add a rectangular body.
@@ -74,26 +74,26 @@ export default class CollisionTestScene extends TestScene {
         });
         game.addBody(body4);  
 
-    //     // PAIR 3
-    //     // Add a rectangular body.
-    //     let body5 : Body = new CircularBody(<CircularBodyDefinition>{
-    //         position: vec2.fromValues(0,-90),
-    //         radius: 10,
-    //         mass: 1.0,
-    //         velocity: vec2.fromValues(-30.0, 0)
-    //     });
-    //     game.addBody(body5);
+        // PAIR 3
+        // Add a rectangular body.
+        let body5 : Body = new CircularBody(<CircularBodyDefinition>{
+            position: vec2.fromValues(0,-90),
+            radius: 10,
+            mass: 1.0,
+            velocity: vec2.fromValues(-30.0, 0)
+        });
+        game.addBody(body5);
 
-    //   // Add a rectangular body.
-    //     let body6 : Body = new RectangularBody(<RectangularBodyDefinition>{
-    //         position: vec2.fromValues(-45,-90),
-    //         width: 20,
-    //         height: 20,
-    //         mass: 1.0,
-    //         velocity: vec2.fromValues(30.0, 0.0),
-    //         torque: 5.0
-    //     });
-    //     game.addBody(body6);
+      // Add a rectangular body.
+        let body6 : Body = new RectangularBody(<RectangularBodyDefinition>{
+            position: vec2.fromValues(-45,-90),
+            width: 20,
+            height: 20,
+            mass: 1.0,
+            velocity: vec2.fromValues(30.0, 0.0),
+            torque: 5.0
+        });
+        game.addBody(body6);
 
 
         // Add ground.
@@ -106,29 +106,29 @@ export default class CollisionTestScene extends TestScene {
         });
         game.addBody(ground);
 
-        // // Add falling balls.
-        // let ball1 : Body = new CircularBody(<CircularBodyDefinition>{
-        //     position: vec2.fromValues(-190,90),
-        //     radius: 10,
-        //     mass: 1.0,
-        //     force: vec2.fromValues(0.0, -10.0),
-        //     collisionCallback: checkCollisionCallback
-        // });
-        // game.addBody(ball1);
-        // let ball2 : Body = new CircularBody(<CircularBodyDefinition>{
-        //     position: vec2.fromValues(190,90),
-        //     radius: 10,
-        //     mass: 1.0,
-        //     force: vec2.fromValues(0.0, -10.0),
-        //     collisionCallback: checkCollisionCallback
-        // });
-        // game.addBody(ball2);
+        // Add falling balls.
+        let ball1 : Body = new CircularBody(<CircularBodyDefinition>{
+            position: vec2.fromValues(-190,90),
+            radius: 10,
+            mass: 1.0,
+            force: vec2.fromValues(0.0, -10.0),
+            collisionCallback: checkCollisionCallback
+        });
+        game.addBody(ball1);
+        let ball2 : Body = new CircularBody(<CircularBodyDefinition>{
+            position: vec2.fromValues(190,90),
+            radius: 10,
+            mass: 1.0,
+            force: vec2.fromValues(0.0, -10.0),
+            collisionCallback: checkCollisionCallback
+        });
+        game.addBody(ball2);
 
-        // body1.onUpdate(backAndForthUpdateCallback(200, body2));
-        // body2.onUpdate(backAndForthUpdateCallback(400, body1));
+        body1.onUpdate(backAndForthUpdateCallback(200, body2));
+        body2.onUpdate(backAndForthUpdateCallback(400, body1));
         body3.onUpdate(backAndForthUpdateCallback(400, body4));
         body4.onUpdate(backAndForthUpdateCallback(400, body3));
-        // body5.onUpdate(backAndForthUpdateCallback(400, body6));
-        // body6.onUpdate(backAndForthUpdateCallback(400, body5));
+        body5.onUpdate(backAndForthUpdateCallback(400, body6));
+        body6.onUpdate(backAndForthUpdateCallback(400, body5));
     }
 }
